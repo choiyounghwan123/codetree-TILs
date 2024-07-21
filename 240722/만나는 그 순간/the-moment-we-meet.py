b6485f3db9b5f9d9624d00aba088b587fa1a6eac
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-07-22 02:01:54 +0900

    Title: 만나는 그 순간, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240722/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,27 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+a, b = [0], [0]
+
+for _ in range(N):
+    d, t = map(str, sys.stdin.readline().split())
+
+    for i in range(int(t)):
+        if d == "L":
+            a.append(a[-1] - 1)
+        else:
+            a.append(a[-1] + 1)
+
+for _ in range(M):
+    d, t = map(str, sys.stdin.readline().split())
+
+    for i in range(int(t)):
+        if d == "L":
+            b.append(b[-1] - 1)
+        else:
+            b.append(b[-1] + 1)
+
+for i in range(1,len(a)):
+    if a[i] == b[i]:
+        print(i)
+        exit()
