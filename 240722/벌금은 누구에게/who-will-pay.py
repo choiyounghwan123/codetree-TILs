41317502eeabd3fc0e0ebeb3df47f19b291d02ac
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-07-22 02:20:07 +0900

    Title: 벌금은 누구에게, Time: 114ms, Memory: 28MB, Status: Passed - CodeTree

--- 240722/벌금은 누구에게/who-will-pay.py
+++ 240722/벌금은 누구에게/who-will-pay.py
@@ -4,11 +4,11 @@ n,m,k = map(int,sys.stdin.readline().split())
 b = [0] * (n+1)
 
 for _ in range(m):
-    b[int(sys.stdin.readline())] += 1
-
-for i in range(n+1):
-    if b[i] == k:
-        print(i)
+    a = int(sys.stdin.readline())
+    b[a] += 1
+    if b[a] == k:
+        print(a)
         exit()
+
 
 print("-1")
