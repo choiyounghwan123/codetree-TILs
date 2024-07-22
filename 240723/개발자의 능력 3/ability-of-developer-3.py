89e9541dc560083d1325228528111bc36f728573
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-07-23 00:11:59 +0900

    Title: 개발자의 능력 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,20 @@
+# 개발자의 능력 3
+
+import sys
+
+nums = list(map(int, sys.stdin.readline().split()))
+
+
+def get_diff(i, j, k):
+    sum1 = nums[i] + nums[j] + nums[k]
+    sum2 = sum(nums) - sum1
+    return abs(sum1 - sum2)
+
+min_diff = 100
+
+for i in range(6):
+    for j in range(i+1,6):
+        for k in range(j+1,6):
+            min_diff = min(get_diff(i,j,k),min_diff)
+
+print(min_diff)
